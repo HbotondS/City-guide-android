5a44cd8bbeb7f1ce5dad6dc8cf94fe7034ef8f44
Hegyi Botond <hbotond97@gmail.com>
2018-05-13 22:54:13 +0200

    Féléves projekt
    Space invaders | Java

---
+++ Space_Invaders/src/sample/interfaces/canShoot.java
@@ -0,0 +1,6 @@
+package sample.interfaces;
+
+public interface canShoot {
+	void shoot();
+
+}
---
+++ Space_Invaders/src/sample/interfaces/moveable.java
@@ -0,0 +1,6 @@
+package sample.interfaces;
+
+public interface moveable {
+	public void update();
+	public void update(double time);
+}
